4f74a72b3d676c24561b2435e4d89e3189cf4ceb
Kamaraj Mani <kamarajitm@gmail.com>
2024-09-23 15:56:04 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,22 @@
+package org.smart;
+import java.util.Scanner;
+
+public class scanner {
+	public static void main(String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Enter the name");
+		String b = sc.nextLine();
+		System.out.println(b);
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
