c35ccec9630f6801acb5d14939578f333ee218eb
Kamaraj Mani <kamarajitm@gmail.com>
2024-09-24 11:41:41 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,28 @@
+package org.smart;
+
+public class ifelse {
+	
+	private void voter(int age) {
+		
+		if(age>=18) {
+			
+			System.out.println("ok");
+		}
+			
+			else {
+				System.out.println("not ok");
+			
+		}
+	}
+			public static void main(String[]args) {
+				
+				ifelse a = new ifelse();
+				
+				a.voter(15);
+				
+			}
+			 
+		}
+	
+
+
---
+++ oddeven.java
@@ -0,0 +1,32 @@
+package org.smart;
+
+public class oddeven {
+	
+
+	 private void oddoreven(int num) {
+			
+			if(num % 2==0) {
+				
+				System.out.println("even number");
+			}
+				
+				else {
+					System.out.println("odd number");
+				
+			}
+		}
+				public static void main(String[]args) {
+					
+					oddeven a = new oddeven();
+					
+					a.oddoreven(18);
+					
+				}
+				 
+			}
+		
+
+
+
+
+
