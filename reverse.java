edb675a44b14dc04c6c238e277eed9e89cd3800c
Kamaraj Mani <kamarajitm@gmail.com>
2024-09-26 16:21:47 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,14 @@
+package org.smart;
+
+public class reverse {
+	
+	public static void main(String[]args) {
+		
+		String a = "KAMARAJ";
+		for (int i=a.length()-1;i>=0;i--) {
+			System.out.println(a.charAt(i));
+			
+		}
+	}
+
+}
